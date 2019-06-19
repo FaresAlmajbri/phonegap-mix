2011f36e75187e9ff65197f8dbf1d314961daea3
Fares Younis SH <fares.younis@outlook.com>
2019-06-19 16:20:16 +0200

    GUI Design

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -18,3 +18,15 @@ function onDeviceReady() {
         }
      );
 }
+function f() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        });
+}
