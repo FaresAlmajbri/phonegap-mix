74dd7667f27fa0c717e1fd9545c8ceba7f235bc5
Fares Younis SH <fares.younis@outlook.com>
2019-06-30 17:25:29 +0200

    backup

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
     document.addEventListener("deviceready", onDeviceReady, false);
 });
 
-function onDeviceReady() {      
-     $('#scan').click( function() 
+function onDeviceReady() {
+    console.log('request got here!');
+     $('#scan').click( function()
         {
+
           cordova.plugins.barcodeScanner.scan(
           function (result) {
               alert("We got a barcode\n" +
                     "Result: " + result.text + "\n" +
                     "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);            
-          }, 
+                    "Cancelled: " + result.cancelled);
+          },
           function (error) {
               alert("Scanning failed: " + error);
           });
